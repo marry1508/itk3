dd5dcd7882f9b057f0b92b2e100e46782782d9b7
Jonah Lüdemann <jonah.luedemann@gmail.com>
2024-07-09 12:16:38 +0200

    fahrpreisberechnung

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ def neuerKunde(vorname,nachname):
     c.execute(f'insert into kunde (vorname,nachname) values ("{vorname}","{nachname}");')
     conn.commit()
 
-# Einen neuen Escooter in der Datenbank anlegen
-def neuerEscooter(standort,mietpreis_strecke,mietpreis_zeit):
-    c.execute(f'insert into escooter (standort,mietpreis_strecke,mietpreis_zeit) values ("{standort}",{mietpreis_strecke},{mietpreis_zeit});')
-    conn.commit()
+# Einen neuen Kunden mit Eingaben aus der Konsole anlegen
+def neuerKundeKonsole():
+    vorname = input("Wie lautet der Vorname?")
+    nachname = input("Wie lautet der Nachname?")
+    neuerKunde(vorname,nachname)
 
 # Alle Kundeneinträge aus der Datenbank ausgeben
 def kundenAusgeben():
@@ -36,6 +37,21 @@ def kundenAusgeben():
         print(f"KundenID: {row[0]}  Name: {row[1]} {row[2]}")
     print()
 
+
+
+
+# Einen neuen Escooter in der Datenbank anlegen
+def neuerEscooter(standort,mietpreis_strecke,mietpreis_zeit):
+    c.execute(f'insert into escooter (standort,mietpreis_strecke,mietpreis_zeit) values ("{standort}",{mietpreis_strecke},{mietpreis_zeit});')
+    conn.commit()
+
+# Einen neuen Escooter mit Eingaben aus der Konsole anlegen
+def neuerEscooterKonsole():
+    standort = input("Wo ist der Escooter?")
+    mietpreis_strecke = input("Wie teuer pro Kilometer?")
+    mietpreis_zeit = input("Wie teuer pro Minute?")
+    neuerEscooter(standort,mietpreis_strecke,mietpreis_zeit)
+
 # Alle Escootereinträge aus der Datenbank ausgeben
 def escooterAusgeben():
     select_query = 'SELECT * FROM escooter'
@@ -49,30 +65,25 @@ def escooterAusgeben():
         print(f"ScooterID: {row[0]}  Standort: {row[1]}  Preis pro Minute: {row[2]}€  Preis pro km: {row[3]}€")
     print()
 
-# Alle Mietvorgänge aus der Datenbank ausgeben
-def mietvorgängeAusgeben():
-    select_query = 'SELECT * FROM mietvorgang'
-    c.execute(select_query)
 
-    # Alle Zeilen abrufen
-    rows = c.fetchall()
-
-    # Daten ausgeben
-    for row in rows:
-        print(f"ID: {row[0]}  ScooterID: {row[1]}  KundenID: {row[2]} Startzeit: {row[3]}  Endzeit: {row[4]}  Strecke: {row[5]}km  Preis: {row[6]}€")
-
-# Einen neuen Kunden mit Eingaben aus der Konsole anlegen
-def neuerKundeKonsole():
-    vorname = input("Wie lautet der Vorname?")
-    nachname = input("Wie lautet der Nachname?")
-    neuerKunde(vorname,nachname)
-
-# Einen neuen Escooter mit Eingaben aus der Konsole anlegen
-def neuerEscooterKonsole():
-    standort = input("Wo ist der Escooter?")
-    mietpreis_strecke = input("Wie teuer pro Kilometer?")
-    mietpreis_zeit = input("Wie teuer pro Minute?")
-    neuerEscooter(standort,mietpreis_strecke,mietpreis_zeit)
+def fahrpreisBerechnen():
+    scooterID = input("Welche ScooterID?")
+    while True:
+        bezahlart = input("Welche Bezahlart? km oder min ")
+        if bezahlart == "km":
+            c.execute(f"select mietpreis_strecke from escooter where scooter_ID = {scooterID}")
+            preis = c.fetchone()
+            strecke = Decimal(input("Wie weit fährst du? "))
+            endpreis = round(preis[0] * strecke,2)
+            return endpreis
+        elif bezahlart == "min":
+            c.execute(f"select mietpreis_zeit from escooter where scooter_ID = {scooterID}")
+            preis = c.fetchone()
+            zeit = Decimal(input("Wie lange möchtest du den Scooter mieten? "))
+            endpreis = round(preis[0] * zeit,2)
+            return endpreis
+        else:
+            print("Ungültige Eingabe")
 
 # Einen neuen Mietvorgang in der Datenbank anlegen
 def neuerMietvorgang():
@@ -101,32 +112,48 @@ def neuerMietvorgang():
             conn.commit()
             break
         else:
-            print("Falsche Eingabe")
+            print("Ungültige Eingabe")
+
+# Alle Mietvorgänge aus der Datenbank ausgeben
+def mietvorgängeAusgeben():
+    select_query = 'SELECT * FROM mietvorgang JOIN Kunde ON kunde.kunden_ID = mietvorgang.kunden_ID order by mietvorgang.mietvorgang_ID asc'
+    c.execute(select_query)
+
+    # Alle Zeilen abrufen
+    rows = c.fetchall()
+
+    # Daten ausgeben
+    for row in rows:
+        print(f"ID: {row[0]}  ScooterID: {row[1]}  KundenID: {row[2]} Name: {row[8]} {row[9]}  Startzeit: {row[3]}  Endzeit: {row[4]}  Strecke: {row[5]}km  Preis: {row[6]}€")
+
 
 
 # Schleife zum abfragen aller vom User gewünschten Eingaben
 while True:
     print('''Was möchtest du machen?
-              Neuen Kunden anlegen - 1
-              Neuen Escooter anlegen - 2
-              Neuen Mietvorgang anlegen - 3
-              Beenden - 4''')
+              Fahrpreis berechnen - 1                
+              Neuen Kunden anlegen - 2
+              Neuen Escooter anlegen - 3
+              Neuen Mietvorgang anlegen - 4
+              Beenden - 5''')
     x = input()
     if x == "1":
-        neuerEscooterKonsole()
+        print(f"{fahrpreisBerechnen()}€")
     elif x == "2":
         neuerEscooterKonsole()
     elif x == "3":
-        neuerMietvorgang()
+        neuerEscooterKonsole()
     elif x == "4":
-        break
+        neuerMietvorgang()
     elif x == "5":
-        mietvorgängeAusgeben()
+        break
     elif x == "6":
+        mietvorgängeAusgeben()
+    elif x == "7":
         kundenAusgeben()
         escooterAusgeben()
     else:
-        print("Ungueltige Eingabe")
+        print("Ungültige Eingabe")
 
 # Ab hier random Befehle zum Testen von Dingen, nicht direkt relevant
 
