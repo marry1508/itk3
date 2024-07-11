3d33a32f8a7491393c944b00a7c8f30a589a9c06
Jonah Lüdemann <jonah.luedemann@gmail.com>
2024-07-11 14:55:55 +0200

    Datenbank füllen aktualisiert

--- filldatabase.sql
+++ filldatabase.sql
@@ -1,15 +1,15 @@
 use scootech;
 
-insert into escooter (standort,mietpreis) values ("Hamburg",0.30);
+insert into escooter (standort,mietpreis) values ("Wilhelmsburg",0.30);
 
-insert into escooter (standort,mietpreis) values ("Berlin",0.40);
+insert into escooter (standort,mietpreis) values ("Wedel",0.40);
 
-insert into escooter (standort,mietpreis) values ("Bayern",0.70);
+insert into escooter (standort,mietpreis) values ("Blankenese",0.70);
 
-insert into kunde (vorname,nachname,passwort) values ("Hans-Peter","Wurst","123");
+insert into kunde (vorname,nachname,passwort) values ("Jonah","Lüdemann","123");
 
-insert into kunde (vorname,nachname,passwort) values ("Max","Mustermann","passwort");
+insert into kunde (vorname,nachname,passwort) values ("Vahan","Telunz","123");
 
-insert into kunde (vorname,nachname,passwort) values ("Mohammed","Etwas","qwertz");
+insert into kunde (vorname,nachname,passwort) values ("Marie","Wolff","test");
 
 commit work;
